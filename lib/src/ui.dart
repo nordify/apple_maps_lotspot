dd37edd03082c2c98dd1172d01a2bc7ece009a2a
Daniel Lozano <daniellozano@Daniels-MacBook-Air.local>
2023-10-11 16:48:38 -0300

    expand map types

--- lib/src/ui.dart
+++ lib/src/ui.dart
@@ -5,6 +5,7 @@
 part of apple_maps_flutter;
 
 /// Type of map tiles to display.
+/// Order matters
 enum MapType {
   /// Normal tiles (traffic and labels, subtle terrain information).
   standard,
@@ -14,6 +15,15 @@ enum MapType {
 
   /// Hybrid tiles (satellite images with some labels/overlays)
   hybrid,
+
+  /// A satellite image of the area with flyover data where available.
+  satelliteFlyover,
+
+  /// A hybrid satellite image with flyover data where available.
+  hybridFlyover,
+
+  /// A street map where MapKit emphasizes your data over the underlying map details.
+  mutedStandard,
 }
 
 enum TrackingMode {
